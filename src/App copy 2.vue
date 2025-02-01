52860e5081eb7a0d6277525bda78367ba935af14
Tyra <68145311+Tyrarose@users.noreply.github.com>
2025-02-02 00:25:14 +0800

    first commit

---
+++ src/App copy 2.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { ref, onMounted }  from 'vue';
+
+const name = ref('John Doe');
+const status = ref('active');
+const tasks = ref(['task1', 'task2', 'task3']);
+const newTask = ref('');
+
+
+const toggleStatus = () => {
+  // status === 'active' ? status = 'inactive' : status = 'active';
+  if(status.value === 'active') {
+    status.value = 'pending'
+  } 
+  else if(status.value === 'pending') {
+    status.value = 'inactive'
+  }
+  else {
+    status.value = 'active'
+  }
+}
+
+const addTask = () => {
+  if (newTask.value.trim() != ''){
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+  }
+}
+
+const deleteTask = (index) => {
+  tasks.value.splice(index,1);
+}
+
+onMounted(async () => {
+  try{
+    const response = await fetch('https:/jsonplaceholder.typicode.com/todos');
+    const data = await response.json();
+    tasks.value = data.map((task) => task.title);
+
+  } catch (error) {
+    console.log('Eror fetching tasks');
+    
+  }
+})
+</script>
+
+<template>
+  <h1>{{name }}</h1>
+  <p v-if="status === 'active' ">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+    <button type="submit">Submit</button>
+  </form>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
+  </ul>
+
+  <button @click="toggleStatus">change status</button>
+
+</template>
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+
+</style>
---
+++ src/App copy.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      name: 'jonn doe',
+      status: 'pending',
+      tasks: [
+        'Task one',
+        'Task two',
+        'Task three'
+      ],
+      link: 'https://www.google.com'
+    }
+  },
+  methods: {
+    toggleStatus() {
+      // this.status = 'active'
+      if(this.status === 'active') {
+        this.status = 'pending'
+      } 
+      else if(this.status === 'pending') {
+        this.status = 'inactive'
+      }
+      else {
+        this.status = 'active'
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{name }}</h1>
+  <p v-if="status === 'active' ">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <!-- <a v-bind:href="link">Google</a> -->
+  <a :href="link">Googlee</a>
+  <br/>
+  <!-- <button v-on:click="toggleStatus">change status</button> -->
+  <button @click="toggleStatus">change status</button>
+
+</template>
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+
+</style>
